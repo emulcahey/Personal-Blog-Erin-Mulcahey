5581d6b5a045fa76cfb77cf13bccefca4ac1a564
emulcahey <e.yehaclum@gmail.com>
2024-04-06 22:27:56 -0500

    created local storage and show all items in it, added some more light dark mode code

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -3,4 +3,38 @@ function lightDarkMode() {
     element.classList.toggle("dark-mode-body");
     element = document.querySelector(".navBoxBorder");
     element.classList.toggle("dark-mode-navBoxBorder");
+    element = document.querySelector(".mainContainerBorders");
+    element.classList.toggle("dark-mode-mainContainerBorders");
+    element = document.querySelector(".newDiv");
+    element.classList.toggle("dark-mode-newDiv");
+    element = document.querySelector(".footerBoxBorder");
+    element.classList.toggle("dark-mode-footerBoxBorder");
+    element = document.querySelector(".postTitle");
+    element.classList.toggle("dark-mode-postTitle");
 }
+
+document.addEventListener("DOMContentLoaded", function(e) {
+    e.preventDefault();
+
+    for (let i = 0; i < localStorage.length; i++){
+        var blogPostInfo = JSON.parse(localStorage.getItem(localStorage.key(i)));
+
+        // Create a new div element
+        var newDiv = document.createElement('div');
+        newDiv.classList.add('newDiv');
+        newDiv.innerHTML = `
+            <div class="postTitle">
+                <h3>${blogPostInfo.title}</h3>
+            </div>
+            <div class="postContent">
+                <p>${blogPostInfo.content}</p>
+            </div>
+            <div class="postUsername">
+                By ${blogPostInfo.username}</p>
+            </div>` 
+            ;
+
+        // Append the new div to the main container
+        document.getElementById('mainContainer').appendChild(newDiv);
+    }
+});
--- assets/js/form.js
+++ assets/js/form.js
@@ -6,3 +6,31 @@ function lightDarkMode() {
     element = document.querySelector(".navBoxBorder");
     element.classList.toggle("dark-mode-navBoxBorder");
 }
+
+//takes form entries and writes to local memory
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission refresh
+    
+    // Get form values
+    var username = document.getElementById('username').value;
+    var title = document.getElementById('title').value;
+    var content = document.getElementById('content').value;
+
+    var timestamp = Math.floor(Date.now());
+    const blogPostItems = {
+        "username": username,
+        "title": title,
+        "content": content
+    }
+
+    localStorage.setItem(`blogPostItems_${timestamp}`, JSON.stringify(blogPostItems));
+    
+    //brings user to blog page
+    location.href = "../../blog.html";
+
+    // Clear the form fields
+    document.getElementById('username').value = '';
+    document.getElementById('title').value = '';
+    document.getElementById('content').value = '';
+
+});
